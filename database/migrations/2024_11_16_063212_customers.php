982ba034bc98fe99aff6e3dde8d9cc44a746f9cb
KaineXx3 <merckainx3@gmail.com>
2024-11-16 14:55:05 +0800

    The customer table is at database/migration/customers.php
    Then we run the command php artisan migrate. The table will be created in mysql

--- database/migrations/2024_11_16_063212_customers.php
+++ database/migrations/2024_11_16_063212_customers.php
@@ -11,7 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('customers',function(Blueprint $table){
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->text('address');
+            $table->string('phoneNumber');
+            $table->string('gender');
+            $table->date('birthday');
+        });
     }
 
     /**
@@ -19,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('customers');
     }
 };
